55b710973959dbeb5e1b98326e56128698f748b9
1dam.m <1dam.m@AULA517.cenecmalaga.net>
2017-01-25 13:09:11 +0100

    Modificacion Ejercicio01

--- src/Ejercicio_01/Ejercicio01.java
+++ src/Ejercicio_01/Ejercicio01.java
@@ -1,10 +1,60 @@
 package Ejercicio_01;
 
+import java.util.*;
+
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		float centigrado = escorrecto();
+		float far = fahr(centigrado) ;
+		
+		System.out.println("Los grados introducidos son "+far + " Fahrengeins");
+	}
+	public static float escorrecto(){
+		Scanner jr = new Scanner(System.in);
+		String grado ;
+		float grados = 0f;
+		boolean esCorrecto = false;
+		
+		do{
+			System.out.println("Introduce una cantidad");
+			grado = jr.next();
+			if(tryparse(grado)){
+				
+				grados =  Float.parseFloat(grado);
+				esCorrecto = true;
+			}else{
+				
+				esCorrecto = false;
+				System.out.println("ERROR: No introducistes un numero");
+				
+			}
+			
+		}while(esCorrecto == false);
+		jr.close();
+		return grados;
+		
 	}
 
+		public static boolean tryparse(String grado){
+			try {
+				Float.parseFloat(grado);
+				return true;
+			}catch(Exception e){
+				return false;
+			}
+				
+			
+			
+		}
+		public static float fahr(float centigrados){
+			
+			
+			return 32 + (9*(centigrados/5));
+			
+			
+		}
+		
 }
